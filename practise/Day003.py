1ecc74d64d516f9b594346d684bd993765f1d557
APIMasterHarsh0900 <harshcoconutcreameater0900@gmail.com>
2026-07-02 18:20:39 +0530

    Day 003 - Python Data Types

---
+++ practise/Day003.py
@@ -0,0 +1,16 @@
+name = "Harsh"
+age = 26
+height = 5.11
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+print("\n Data Types:")
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
