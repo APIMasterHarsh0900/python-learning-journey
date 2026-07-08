7a0d037029abee42f05ab07d5ef1530499ee0cbe
APIMasterHarsh0900 <harshcoconutcreameater0900@gmail.com>
2026-07-08 18:07:03 +0530

    Day 008 - My functional mastery 2

---
+++ practise/Day009.py
@@ -0,0 +1,9 @@
+def multiplication_table(number):
+    print(f"\nMultiplication Table of {number}\n")
+
+    for i in range(1, 11):
+        print(f"{number} x {i} = {number * i}")
+
+number = int(input("Enter a number: "))
+
+multiplication_table(number)
