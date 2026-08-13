722a13709adc3cff2ebfa760a40653bc23fa65cb
APIMasterHarsh0900 <harshcoconutcreameater0900@gmail.com>
2026-08-14 02:33:46 +0530

    Array - Binary Search via function

--- DSA/BinarySearchFirst&Last.py
+++ DSA/BinarySearchFirst&Last.py
@@ -37,3 +37,26 @@ def searchRange(nums, target):
             high = mid - 1
 
     return [first, last]
+
+def second_occurrence(nums, target):
+    low = 0
+    high = len(nums) - 1
+    first = -1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if nums[mid] == target:
+            first = mid
+            high = mid - 1
+
+        elif nums[mid] < target:
+            low = mid + 1
+
+        else:
+            high = mid - 1
+
+    if first != -1 and first + 1 < len(nums) and nums[first + 1] == target:
+        return first + 1
+
+    return -1
