8834df8162f1001adc4cc5e726334dbf7c2fda18
APIMasterHarsh0900 <harshcoconutcreameater0900@gmail.com>
2026-08-15 05:44:01 +0530

    Array - Two pointers reversing the array via function

--- DSA/Arrays/ReversingTheArray.py
+++ DSA/Arrays/ReversingTheArray.py
@@ -1,15 +1,3 @@
-def is_pallindrome(lst):
-    n=len(lst)
-    i=0
-    j=n-1
-    while i<j:
-        if lst[i]!=lst[j]:
-            return False
-        i=i+1
-        j=j-1
-    return True
-print(is_pallindrome([1,2,3,2,1]))
-
 def reverse_list(lst):
     """
     Function to reverse the order of elements in a list.
