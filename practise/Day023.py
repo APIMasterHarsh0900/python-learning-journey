b4c933617385039cc237008de6850381ec7c63c2
APIMasterHarsh0900 <harshcoconutcreameater0900@gmail.com>
2026-08-15 06:48:29 +0530

    Import module and package via function

---
+++ practise/Day023.py
@@ -0,0 +1,8 @@
+import math
+print(math.sqrt(16))
+import numpy as np
+print(np.array([1, 2, 3, 4, 5]))
+from Package.maths import sum, product, difference
+print(sum(5, 3))
+print(product(5, 3))
+print(difference(5, 3))
