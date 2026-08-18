49bef6fc8cf2e11fbcd5b1fdf7a0f39102cdc164
APIMasterHarsh0900 <harshcoconutcreameater0900@gmail.com>
2026-08-18 17:53:57 +0530

    Max element of array

---
+++ DSA/Arrays/Max_elementOfArray.py
@@ -0,0 +1,9 @@
+def max_element(arr):
+    if not arr:
+        return None  # Return None for empty array
+
+    max_val = arr[0]  # Assume the first element is the maximum
+    for num in arr:
+        if num > max_val:
+            max_val = num  # Update max_val if a larger number is found
+    return max_val  # Return the maximum value found
